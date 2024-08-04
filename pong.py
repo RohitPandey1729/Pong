349d61462edc6600158793f907674ab7e88b0a3f
Rohit Pandey <myboxrohit@gmail.com>
2024-08-04 15:44:04 -0700

    I added some binding to the keyboard and the ball can bounce on one side

--- pong.py
+++ pong.py
@@ -31,31 +31,49 @@ ball.shape("square")
 ball.color("white")
 ball.penup()        
 ball.goto(0, 0)
-
-
-# Pen
-pen = turtle.Turtle()
-pen.speed(0)
-pen.shape("square")
-pen.color("white")
-pen.penup()
-pen.hideturtle()
-pen.goto(0, 260)
-pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+ball.dx = 0.1
+ball.dy = 0.1   
 
 # Functions
 def paddle_a_up():
-    y = paddle_a.ycor()
+    y = paddle_a.ycor()     #know the current y coordinate
     y += 20
     paddle_a.sety(y)
 
+
 def paddle_a_down():
-    y = paddle_a.ycor()
+    y = paddle_a.ycor()     #know the current y coordinate
     y -= 20
     paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()     #know the current y coordinate
+    y += 20
+    paddle_b.sety(y)
 
+
+def paddle_b_down():
+    y = paddle_b.ycor()     #know the current y coordinate
+    y -= 20
+    paddle_b.sety(y)
+
+
+    #Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main game loop
 while True:
     wn.update()
+# Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border check
+    if ball.ycor() > 290:   
+        ball.sety(290)
+        ball.dy *= -1       # Reverses the direction
